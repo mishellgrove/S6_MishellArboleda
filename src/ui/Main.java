11b1ec1fac9c3b08f53b7d4d73b3d07a1ffca421
mishellgrove <70043762+mishellgrove@users.noreply.github.com>
2021-03-12 23:41:16 -0500

    update

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -16,8 +16,22 @@ public class Main {
 				array[i] = sc.nextInt();
 			
 			Arrays.sort(array);
-
-			System.out.println("Peter should buy books whose prices are " );
+			
+			int mon = sc.nextInt();
+			int x = -1;
+			int y = -1;
+			
+			for (int i = 0; i < num; i++) {
+				
+				int req = mon - array[i];
+				int res = Arrays.binarySearch(array,i+1,num,req);
+				if (res >= 0 && (array[res] - array[i] < y - x || (x == -1 && y == -1))) {
+					x = array[i];
+					y = array[res];
+				}
+			}
+			
+			System.out.println("Peter should buy books whose prices are " + x + " and " + y);
 		}
 		sc.close();
 	}
